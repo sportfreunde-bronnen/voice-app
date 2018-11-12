32cfc78ec930cffdaba2d9a77a9b024113634728
Magnus Buk <info@magnusbuk.de>
2018-11-12 14:23:41 +0100

    added repeat and NEW_USER intents

--- app/app.js
+++ app/app.js
@@ -12,15 +12,55 @@ const config = {
 
 const app = new App(config);
 
-
 // =================================================================================
 // App Logic
 // =================================================================================
 
 app.setHandler(
   {
+    /**
+     * Existing user launches the skill
+     *
+     * @constructor
+     */
     'LAUNCH': function() {
-        this.tell('Herzlich willkommen bei den Sportfreunden Bronnen!')
+      this.tell('Herzlich willkommen bei den Sportfreunden Bronnen!')
+    },
+
+    /**
+     * NEW user launches the skill
+     *
+     * @constructor
+     */
+    'NEW_USER': function() {
+      this.tell('JA GRIASDE DU BAURASEGGL!');
+    },
+
+    /**
+     * Global unhandled intent
+     *
+     * @constructor
+     */
+    'Unhandled': function() {
+      this.tell('Das habe ich leider nicht verstanden.');
+    },
+
+    /**
+     * Repeat
+     *
+     * @constructor
+     */
+    'AMAZON.RepeatIntent': function() {
+      this.toIntent('RepeatIntent');
+    },
+
+    /**
+     * Global repeat intent
+     *
+     * @constructor
+     */
+    'RepeatIntent': function() {
+      this.repeat();
     }
   },
 

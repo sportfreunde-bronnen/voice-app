972930c61ab7625ee648cdd526189cf7472ef93f
Magnus Buk <info@magnusbuk.de>
2019-02-28 15:23:08 +0100

    minor break fix

--- app/handler/ClubAnthem.js
+++ app/handler/ClubAnthem.js
@@ -12,6 +12,7 @@ module.exports = {
       .addText('Sie hören die Vereinshymne Blau und Weiß der Sportfreunde Bronnen.')
       .addBreak('250ms')
       .addText('Nach einer freien Interpretation der aktiven Spieler und einiger Gönner.')
+      .addBreak('500ms')
       .addAudio('https://services.sf-bronnen.de/mp3/Blau-und-Weiss_Spieler.mp3');
     return this.tell(speech);
   }
